b490a58eb9ca542b31893757eac0cefcf848d630
fadiquader <fadi@meeopp.com>
2019-05-17 17:12:45 +0800

    add more

--- basics/06-if-then.sh
+++ basics/06-if-then.sh
@@ -2,6 +2,47 @@
 # Author: Fadi Quader
 # Description: if-then
 
+$condition = 2
+if [ $condition -eq 1 ]
+then
+    # todo code
+else
+    # todo code
+fi
+
+# Another format
+if [ $condition -eq 1 ]; then
+    # todo code
+elif [ $condition -eq 2 ]; then
+    # todo code
+else
+    # todo code
+fi
+
+# Operands (numbers) => -eq (equal to), -ne, -gt (greater than), -lt (less than), -ge, -le
+
+# And with if statement
+if [ $condition -lt 10 ] && [ $condition -gt 1 ]; then
+fi
+
+# Or with if statement
+if [ $condition -lt 10 ] || [ $condition -gt 1 ]; then
+fi
+
+# Negative if statement
+if [ ! $condition -lt 10 ]; then
+fi
+
+# String comparison
+# [ "$str1" = "$STR2" ]
+# [ "$str1" != "$STR2" ]
+# to not using "" user => [[ $STR1 = $STR2 ]]
+# test if string is empty [ -z "$STR!" ]
+# test if string is non-empty [ -n "$STR!" ]
+# Alphabetically compare two string [[ $STR1 > $STR2 ]]
+# Wildcards [[ $STR1 =~ $regex ]]
+
+# Examples
 count=100
 if [ $count -eq 100 ]
 then
